88ad78a743398a69e9cfcc5ea6815a6ce78d5a3a
Shefa1122 <shilpozone1@gmail.com>
2025-08-25 11:07:50 +0600

    changes in comment

--- lib/product_detail_page.dart
+++ lib/product_detail_page.dart
@@ -24,7 +24,7 @@ class ProductDetailPage extends StatelessWidget {
             },
             child: const Text("Add to Cart"),
           ),
-        ],
+        ],//bhungchung
       ),
     );
   }
